c649567bc5a3b2f167debd4822945513744c1bde
Mohammad Hossein Amirpanahi <mhap75@yahoo.com>
2023-01-26 01:10:34 +0330

    fix app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,11 @@ $(document).ready(function () {
 
     $('.acordion ul > li').on('click', function () {
         $('.acordion p').slideUp(700);
+        $('.acordion h4').css('font-weight', "inherit");
         $('.acordionToggleBtn').removeClass('rotated');
         if ($(this).find('p').css('display') == 'none') {
             $(this).find('p').slideToggle(700);
+            $(this).find('h4').css('font-weight', "700");
             $(this).find('.acordionToggleBtn').addClass('rotated');
         };
     });
